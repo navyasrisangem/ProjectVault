630a754aad721a42b398fbcdeb7bc43d7a5bb417
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2024-12-25 20:39:41 +0530

    weather app changes in js code

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,37 +1,74 @@
-document.addEventListener("DOMContentLoaded",()=> {
+document.addEventListener("DOMContentLoaded", () => {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=";
     const frontPage = document.getElementById("frontPage");
+
     const section = document.querySelector("section");
     const footer = document.querySelector("footer");
-    frontPage.classList.remove("d-none");
-    section.classList.add("d-none");
-    footer.classList.add("d-none");
     let buttonClick = document.getElementById("buttonClick");
-    
-    buttonClick.addEventListener("click", () =>{
+    const errorMsg = document.querySelector("#errorMsg");
+    const searchInput = document.getElementById("searchInput");
+    errorMsg.classList.add("d-none");
+
+    buttonClick.addEventListener("click", getWeather);
+    searchInput.addEventListener("keydown", (e)=> {
+         if(e.key === "Enter") {
+            getWeather();
+         }
+    })
+        function getWeather() {
         frontPage.classList.add("d-none");
         const cityName = document.getElementById("cityName");
-        const searchInput = document.getElementById("searchInput");
+        
         const temperature = document.getElementById("temperature");
         const humidity = document.getElementById("humidity");
         const wind = document.getElementById("wind");
-        
-        cityName.innerHTML = searchInput.value.trim();
-        
-        let newurl = `${url}${searchInput.value.trim()}&appid=bc464ef9370066684fba722fe0486755&units=metric`;
+
+        let val = searchInput.value.trim();  
+        if (val === "") {
+            errorMsg.classList.remove("d-none");
+            section.classList.add("d-none");
+            footer.classList.add("d-none");
+            return;
+        }
+
+        let newurl = `${url}${val}&appid=bc464ef9370066684fba722fe0486755&units=metric`;
         fetch(newurl)
-        .then((response) => response.json())
-        .then(data => {
-        console.log(data);
-        temperature.innerHTML = data.main.temp + "°C";
-        humidity.innerHTML = data.main.humidity + "%";
-        wind.innerHTML = data.wind.speed + "m/s";
-        })
-        section.classList.remove("d-none");
-        footer.classList.remove("d-none");
-    })
+            .then((response) => response.json())
+            .then(data => {
+                if (data.cod !== 200) {
+                    errorMsg.classList.remove("d-none");
+                    section.classList.add("d-none");
+                    footer.classList.add("d-none");
+                } else {
+                    const temp = data.main.temp;
+                    temperature.innerHTML = `${temp}°C`;
+                    humidity.innerHTML = `${data.main.humidity}%`;
+                    wind.innerHTML = `${data.wind.speed}m/s`;
+                    cityName.innerHTML = `${data.name}`;
+
+                    updateImage(temp);
+
+                    errorMsg.classList.add("d-none");
+                    section.classList.remove("d-none");
+                    footer.classList.remove("d-none");
+                } 
+
+            })
+            function updateImage(temp) {
+            let sectionImg = document.getElementById("sectionImg");
+               if(temp<=5) {
+                 sectionImg.src = "assets/rainy.png";
+               } else if(temp>5 && temp<=15) {
+                sectionImg.src = "assets/cloudy.png";
+               } else if(temp>15 && temp<=25) {
+                sectionImg.src = "assets/moderate.png";
+               } else {
+                sectionImg.src = "assets/sunny.png";
+               }
+            }
+    }
 })
 
 
 
-    
+
