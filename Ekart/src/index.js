542e4ac1cb7bb54aface8f7deabe58e70b98d04f
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2025-02-28 15:46:30 +0530

    Ekart

---
+++ Ekart/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import bootstrap from 'bootstrap';
+import { Fakestore } from './components/fakestore';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
+    <Fakestore />  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
